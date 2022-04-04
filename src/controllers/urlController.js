d6260ac2eb63f98cb8115f33ad1db42948ad9555
pramodjadhav09 <impramodj@gmail.com>
2022-04-04 20:36:40 +0530

    phase 1 done

---
+++ src/controllers/urlController.js
@@ -0,0 +1,89 @@
+const validUrl = require('valid-url')
+const shortId = require('shortid')
+const urlModel = require("../models/urlModel")
+
+
+
+//VALIDATION_FUNCTIONS-
+
+//Function 1-
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0
+}
+
+//Function 2-
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'String' && value.trim().length === 0) return false
+    return true
+}
+
+
+
+
+//CREATE_URL-
+const createUrl = async function (req, res) {
+    try {
+
+        let data = req.body;
+
+        //validations-
+        if (!isValidRequestBody(data)) {
+            return res.status(400).send({ status: false, msg: "please provide some input to create data" })
+        }
+
+        if (!isValid(data.longUrl)) {
+            return res.status(400).send({ status: false, msg: "please provide a url" })
+        }
+
+        //validating with valid-url package-
+        if (!validUrl.isUri(data.longUrl)) {
+            return res.status(400).send({ status: false, msg: "please provide a valid url" })
+        }
+
+
+
+        //generating urlCode with shortId package
+        let urlCode = shortId.generate().toLowerCase()
+        //creating shortUrl with base url & urlCode
+        let shortUrl = `http://localhost:3000/${urlCode}`
+
+        data.urlCode = urlCode
+        data.shortUrl = shortUrl
+
+
+        let createNewUrl = await urlModel.create(data);
+        //console.log(createNewUrl)
+        return res.status(201).send({ status: true, message: "url created successfully", data: createNewUrl })
+
+    } catch (err) {
+        return res.status(500).send({ status: false, error: err.message })
+    }
+}
+
+
+
+
+//getAPIforRedirect-
+
+const getUrlData = async function (req, res) {
+    try {
+        let data = req.params.urlCode;
+        //console.log(data);
+
+        let getData = await urlModel.findOne({ urlCode: data })
+        //console.log(getData);
+        if (!getData) { return res.status(404).send({ status: false, message: "no short url available for your request" }) }
+
+        return res.status(301).redirect(getData.longUrl)
+    } catch (err) {
+        return res.status(500).send({ status: false, error: err.message })
+    }
+}
+
+
+
+
+
+module.exports.createUrl = createUrl
+module.exports.getUrlData = getUrlData
