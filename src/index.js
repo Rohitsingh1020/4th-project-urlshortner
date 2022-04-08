64cff3a79cae9c1f08f8577b1d367923a338bc27
Rohit <rohit.rajsingh.8220@gmail.com>
2022-04-08 15:53:31 +0530

    solutin updates

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const route = require('../src/routes/route')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-mongoose.connect("mongodb+srv://Pramod09:Pramod%4010@pramod09.i3td2.mongodb.net/pramod09-db", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://Projectblog1:Roomno20@cluster0.vl9g6.mongodb.net/rohitkumarsingh-DB?authSource=admin&replicaSet=atlas-fm8m4t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", { useNewUrlParser: true })
 
     .then(() => console.log("MongoDB is connected"))
     .catch(err => console.log(err))
